1aef19fda6597701b12a6e3387d85876a04da71b
Hammad Hussain <ham2qur@hotmail.com>
2022-03-06 03:10:51 +0500

    Added boilercode for odoo module

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Barcode Auto (EAN13)',
+    'version': '14.0.0.2',
+    'summary': "Generate EAN13 barcode based on internal reference. EAN13 supports product quanity using barcode nomenclature.",
+    'category': 'Sales',
+    'description': """
+""",
+    'author': 'ham2qur',
+    'website': 'http://www.tradetec.info',
+    'depends': ['base', 'product', 'sale', 'barcodes','sale_management'],
+    'data': [
+        'views/generate_product_ean13_view.xml',
+        'views/res_config_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
